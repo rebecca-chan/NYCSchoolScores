bc69879b1953d79ee64e25e7b259be9c80e1cd80
Rebecca Chan <rebeccachan@rebeccas-mbp-2.lan>
2023-02-28 18:15:54 -0500

    add app token header

--- app/src/main/java/com/example/nycschoolscores/api/SchoolsService.kt
+++ app/src/main/java/com/example/nycschoolscores/api/SchoolsService.kt
@@ -1,18 +1,39 @@
 package com.example.nycschoolscores.api
 
+import okhttp3.Interceptor
+import okhttp3.OkHttpClient
+import retrofit2.Response
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 object SchoolsService {
 
-    private const val BASE_URL =  "https://data.cityofnewyork.us/resource/"
+    private const val BASE_URL = "https://data.cityofnewyork.us/resource/"
 
-    // TODO: use okhttpclient to cache network results
+    private val client: OkHttpClient = OkHttpClient.Builder()
+        .addInterceptor(AuthenticationInterceptor()).build()
 
     val api: SchoolsApi by lazy {
-         Retrofit.Builder()
-             .baseUrl(BASE_URL)
-             .addConverterFactory(GsonConverterFactory.create())
-             .build().create(SchoolsApi::class.java)
+        Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .client(client)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build().create(SchoolsApi::class.java)
+    }
+}
+
+class AuthenticationInterceptor : Interceptor {
+
+    override fun intercept(chain: Interceptor.Chain): okhttp3.Response {
+
+        val newRequest = chain.request().newBuilder()
+            .addHeader("X-App-Token", APP_TOKEN)
+            .build()
+
+        return chain.proceed(newRequest)
+    }
+
+    companion object {
+        const val APP_TOKEN = "efusd7GOL1wTJV6y8XOORo9ow"
     }
 }
